fcee9a0a099ccf86d89e2cc641c6fa817d5dc9a3
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2023-09-26 19:08:50 +0200

    inserted search logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,26 @@
+import React from 'react';
 import '../Styles/input.css'
 
 interface Props{
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.Fc<Props> = ({ todo, setTodo }) => {
-  return <form className='input'>
-    <input 
-    type='input'
-    value={todo}
-    onChange={(e) => setTodo(e.target.value)} 
-    placeholder='Enter a task' 
-    className='input_box'/>
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  return(
+    <form className='input' onSubmit={handleAdd}>
+      <input 
+        type='input'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)} 
+        placeholder='Enter a task' 
+        className='input_box'
+        />
     <button className="input_submit" type="submit">GO</button>
   </form>
+
+  )
 }
 
 export default InputField
