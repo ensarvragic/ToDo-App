389ae01f8d696c4419fd8673fd15c0cc9951dc54
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2023-09-26 18:46:23 +0200

    importing react icons

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,18 @@
 import '../Styles/input.css'
 
-const InputField = () => {
-  return <form>
-    <input type='input' placeholder='Enter a task' className='input_box'/>
+interface Props{
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.Fc<Props> = ({ todo, setTodo }) => {
+  return <form className='input'>
+    <input 
+    type='input'
+    value={todo}
+    onChange={(e) => setTodo(e.target.value)} 
+    placeholder='Enter a task' 
+    className='input_box'/>
     <button className="input_submit" type="submit">GO</button>
   </form>
 }
